2b53b3a44d69cc991c25aa13e3c42b7be854614b
Hamish Poole <hamishgcp@gmail.com>
2021-12-03 18:16:34 +1100

    things2

--- advent_2021/day3/day3_binary.py
+++ advent_2021/day3/day3_binary.py
@@ -1,38 +1,94 @@
+import operator
+
 with open(file="/home/urthor/projects/advent_of_code/advent_2021/day3/input/input",
           mode='r') as input1:
     binary = input1.readlines()
 
-cleaned_bin = [x[:-1] for x in binary]
+data = [x[:-1] for x in binary]
+
+data = ['00100',
+        '11110',
+        '10110',
+        '10111',
+        '10101',
+        '01111',
+        '00111',
+        '11100',
+        '10000',
+        '11001',
+        '00010',
+        '01010']
+
+length = len(data)
 
 # print(cleaned_bin[:5])
 
-result_array = [0 for x in range(len(cleaned_bin[0]))]
+result_array = [0 for x in range(len(data[0]))]
 
 print(len(result_array))
 
-print(len(cleaned_bin))
+print(len(data))
 
-for index in range(len(cleaned_bin[0])):
+for index in range(len(data[0])):
 
-    for number in cleaned_bin:
+    for number in data:
         result_array[index] += int(number[index])
-
-print(result_array)
 
 final_array = list(result_array)
 
-gamma = [str(int(x - 499 > 0)) for x in list(final_array)]
-print(gamma)
+gamma = [str(int(x - length / 2 > 0)) for x in list(final_array)]
 
 str_gamma = int("".join(gamma))
-print(str_gamma)
+
 val = bin(str_gamma)
-print(val, type(val))
-epsi = [str(1 - int(x - 499 > 0)) for x in list(final_array)]
+epsi = [str(1 - int(x - length / 2 > 0)) for x in list(final_array)]
 
 str_epsi = int("".join(epsi))
-print(str_epsi)
 val2 = bin(str_epsi)
-print(val2, type(val2))
 
-print(1300*2795)
+print(gamma, epsi, str_gamma, str_epsi)
+print(1300 * 2795)
+
+for index in range(len(data[0])):
+
+    for number in data:
+        result_array[index] += int(number[index])
+
+# 101011101011
+# 10100010100
+
+oxy_array = list(data)
+prev_oxy = oxy_array
+
+print(str_gamma, type(str_gamma))
+
+max_count_similar = 0
+
+oxy_gen = ''
+co2 = ''
+
+
+def calc_oxco2(operator):
+    max_count_similar = 0
+
+    bin_and = str_gamma & int(number)
+    count_similar = 0
+
+    for digit in str(bin_and):
+        if digit != '1':
+            count_similar += 1
+        else:
+            break
+
+    if operator(count_similar, max_count_similar):
+        max_count_similar = count_similar
+        co2 = number
+
+
+print(oxy_gen, 'here oxy')
+print(co2, 'here co2')
+
+# 001111100111
+# 100000000101
+
+print(0b100000000101 * 0b001111100111)
